3de0afe98ebb148834ddf33e6275b28f4ddd0d43
Roshan Kumar Panjiyara <roshanpanziarya055@gmail.com>
2025-02-11 15:45:16 +0530

    Initial Commit

---
+++ 02 -project/src/App.jsx
@@ -0,0 +1,9 @@
+import Header from "./components/header.jsx";
+
+function App() {
+  return (
+    <Header/>
+  )
+}
+
+export default App
---
+++ 02 -project/src/components/UserInput.jsx
@@ -0,0 +1,12 @@
+export default function UserInput(){
+    return (
+        <section id="user-input">
+            <div className="input-group">
+                <p>
+                    <label htmlFor=""></label>
+                    <input type="text" name="" id="" />
+                </p>
+            </div>
+        </section>
+    );
+}
