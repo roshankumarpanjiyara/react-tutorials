a9b330420ef7f7f9714bea5592931497a2777d83
Roshan Kumar Panjiyara <roshanpanziarya055@gmail.com>
2025-02-18 21:23:41 +0530

    Modified Commit

--- 03-Styling/src/components/Header.jsx
+++ 03-Styling/src/components/Header.jsx
@@ -1,15 +1,11 @@
 import logo from '../assets/logo.png';
-import './Header.css';
 
 export default function Header() {
   return (
-    <header>
+    <header className='flex flex-col items-center mt-8 mb-16'>
       <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p style={{
-        color: 'red',
-        textAlign: 'left',
-      }}>A community of artists and art-lovers.</p>
+      <h1 className='text-red-50'>ReactArt</h1>
+      <p>A community of artists and art-lovers.</p>
     </header>
   );
 }
